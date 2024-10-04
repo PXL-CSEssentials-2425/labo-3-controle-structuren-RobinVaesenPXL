a23c50f8d2c6b14f1907fa21835750fe24447b9b
RobinVaesenPXL <11901804@student.pxl.be>
2024-10-04 11:16:31 +0200

    Labo afgewerkt

--- Labo3KnightVsGoblin/Labo3KnightVsGoblin/Program.cs
+++ Labo3KnightVsGoblin/Labo3KnightVsGoblin/Program.cs
@@ -1,54 +1,113 @@
-﻿/* 
- * Deel 1
- * 
- * We gaan een applicatie maken waarin de speler als ridder tegen een goblin moet vechten.
- * We starten met een controle te implementeren die kijkt of de speler nog leeft.
- *  - Declareer en initialiseer een waarde knightHealth (0) en goblinHealth (20).
- *  - Indien de levenspunten van de speler (knightHealth) kleiner of gelijk zijn aan nul,
- *    dan toon je aan de speler dat hij/zij is gestorven. 
- *  - Doe hetzelfde voor de goblin.
- *  - Extra: gebruik de Random klasse om de levenspuntenvan de ridder en goblin in te stellen.
- */
+﻿Console.WriteLine("Welcome to Knight vs Goblin!");
+Console.WriteLine("----------------------------");
+Console.Write("Enter knight health: ");
+string input = Console.ReadLine();
 
-/* 
- * Deel 2
- * 
- * - Indien de speler niet gestorven is, dan druk je af hoeveel levenspunten (knightHealth) 
- *   speler nog heeft. Gebruik hier else voor.
- * - Laat de speler zelf levenspunten ingeven voor de ridder (knightHealth). Zo kiest elke speler 
- *   zelf hoe moeilijk het gevecht is. 
- * - De waarde knightHealth moeten tussen 0 en 100. Indien de speler een ongeldige waarde ingeeft, 
- *   dan wordt de standaard waarde 100 gebruikt.
- *    - Extra: gebruik TryParse om de input van de speler te verwerken, 
- *      zodat het programma niet zal crashen.
- */
+Random randomNumberGenerator = new Random();
 
-/*
- * Deel 3
- * 
- * Declareer en initialiseer een aanvalswaarde voor de ridder, attackKnight (10), 
- * en de goblin, attackGoblin (5).
- * Laat de speler een actie selecteren door een getal in te geven. Gebruik een switch:
- *  - Als ik als speler actie 1 kies, dan val ik aan en wordt attackKnight afgetrokken van 
- *    goblinHealth. Beschrijf in de output wat er gebeurt.
- *  - Als ik als speler actie 2 kies, dan genees ik mezelf 10 levenspunten. Beschrijf in de 
- *    output wat er gebeurt.
- *  - Als de speler een ongeldige actie ingeeft, dan weergeef je dit in de output.
- *  - Extra: voeg extra acties toe die de speler kan kiezen.
- *  - Extra: zorg er voor dat de goblin ook een actie neemt.
- */
+int knightHealth;
 
-/*
- * Deel 4
- * 
- * Gebruik een for lus, zodat de speler exact 4 keer een actie kan selecteren.
- */
+if (int.TryParse(input, out knightHealth))
+{
+    if (knightHealth <= 0 || knightHealth > 100)
+    {
+        Console.WriteLine("Invalid number, default value 100 is used.");
+        knightHealth = 100;
+    }
+}
+else
+{
+    Console.WriteLine("Invalid number, default value 100 is used.");
+    knightHealth = 100;
+}
 
-/*
- * Deel 5
- * 
- * Vervang de for lus door een while lus die vraagt aan de speler om een actie 
- * uit te voeren zolang als de ridder of goblin nog leeft.
- * 
- * Extra: zorg er voor dat de goblin ook een actie neemt.
- */
+int goblinHealth = randomNumberGenerator.Next(1, 101);
+
+if (knightHealth <= 0)
+{
+    Console.WriteLine("You have died!");
+}
+else
+{
+    Console.WriteLine($"Knight health: {knightHealth}");
+}
+
+if (goblinHealth <= 0)
+{
+    Console.WriteLine("The goblin has died!");
+}
+else
+{
+    Console.WriteLine($"Goblin health: {goblinHealth}");
+}
+Console.WriteLine();
+
+int move;
+int knightAttack = 10;
+int goblinAttack;
+int round = 0;
+
+while (knightHealth > 0 && goblinHealth > 0)
+{
+    round++;
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine($"Round {round}.");
+    Console.ResetColor();
+    goblinAttack = randomNumberGenerator.Next(5, 16);
+
+    Console.WriteLine("Choose an action:\n1. Attack\n2. Heal");
+
+    while (!int.TryParse(Console.ReadLine(), out move))
+    {
+        Console.WriteLine("Please enter an action.\n1. Attack\n2. Heal");
+    }
+    switch (move)
+    {
+        case 1:
+            goblinHealth -= knightAttack;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Goblin was attacked and lost {knightAttack} health!");
+            Console.ResetColor();
+            if (goblinHealth < 0)
+            {
+                goblinHealth = 0;
+            }
+            Console.WriteLine($"Score:\nKnight: {knightHealth} health\nGoblin: {goblinHealth} health");
+            Console.WriteLine();
+            break;
+        case 2:
+            knightHealth += 10;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Knight healed and gained 10 health!");
+            Console.ResetColor();
+            Console.WriteLine($"Score:\nKnight: {knightHealth} health\nGoblin: {goblinHealth} health");
+            Console.WriteLine();
+            break;
+        default:
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Invalid input for knight action");
+            Console.ResetColor();
+            break;
+    }
+    if (goblinHealth > 0)
+    {
+        knightHealth -= goblinAttack;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Knight was attacked and lost {goblinAttack} health!");
+        Console.ResetColor();
+        if (knightHealth < 0)
+        {
+            knightHealth = 0;
+        }
+        Console.WriteLine($"Score:\nKnight: {knightHealth} health\nGoblin: {goblinHealth} health");
+        Console.WriteLine();
+    }
+}
+if (goblinHealth <= 0)
+{
+    Console.WriteLine("Game over. Knight wins.");
+}
+else if (knightHealth <= 0)
+{
+    Console.WriteLine("Game over. Goblin wins.");
+}
